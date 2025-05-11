6e9a6658655b460b9324293022e95ac59bbbdeb7
ujjwal G <crystalninja0123@gmail.com>
2025-05-11 21:55:12 +0530

    hidden the api keys

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,9 @@
 import React from 'react'
+import HomePage from './pages/HomePage'
+import signUpPage from './pages/signuppage'
+import LoginPage from './pages/loginpage'
+import home from './pages/HomePage'
+import home from './pages/HomePage'
 import { Routes,Route } from 'react-router-dom'
 
 const App = () => {
@@ -7,7 +12,7 @@ const App = () => {
       <Navbar/>
 
       <Routes>
-        <Route path='/' element={<Home/>}/>
+        <Route path='/' element={<HomePage/>}/>
         <Route path='/signup' element={<signUpPage/>}/>
         <Route path='/login' element={<LoginPage/>}/>
         <Route path='/settings' element={<SettingsPage/>}/>
