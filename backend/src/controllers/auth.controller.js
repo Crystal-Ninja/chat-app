4bf9711602bdadc14e76666dfcf3b86853005f6c
ujjwal G <crystalninja0123@gmail.com>
2025-06-06 19:46:48 +0530

    fixed profile page errors

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -100,8 +100,8 @@ console.log(req.user);
         }
         const uploadResponse =await cloudinary.uploader.upload(profilePic)
         const updatedUser=await User.findByIdAndUpdate(userId,{profilePic:uploadResponse.secure_url},{new:true})
-        res.status(200).json(UpdatedUser);
-    } catch (error) {
+        res.status(200).json(updatedUser);
+    } catch (error) {   
         console.log("error in update profile", error);
         res.status(500).json({message:"internal server error"})
         
